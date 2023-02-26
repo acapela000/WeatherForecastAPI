f7e5eeb8accad05c2a6df6d237fbf452031c467a
thao <mrtrados@gmail.com>
2023-02-26 23:04:47 +0900

    fix HealthCheck method

--- src/main/java/com/charlieThao/weather_forcast_demo/controller/HealthCheck.java
+++ src/main/java/com/charlieThao/weather_forcast_demo/controller/HealthCheck.java
@@ -1,12 +1,13 @@
 package com.charlieThao.weather_forcast_demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheck {
 
-    @RequestMapping("/health-check")
+    @RequestMapping(value = "/health-check", method = RequestMethod.GET)
     public String checkSttOfAPI() {
         return "OK";
     }
