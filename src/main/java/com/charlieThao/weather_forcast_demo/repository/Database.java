04c7982b8a0bccf7e254447d0e7fb65af8aaa40a
thao <mrtrados@gmail.com>
2023-02-27 17:37:42 +0900

    add getListOfWF Repository

--- src/main/java/com/charlieThao/weather_forcast_demo/repository/Database.java
+++ src/main/java/com/charlieThao/weather_forcast_demo/repository/Database.java
@@ -3,14 +3,18 @@ package com.charlieThao.weather_forcast_demo.repository;
 import com.charlieThao.weather_forcast_demo.model.WeatherForecast;
 
 import java.util.HashMap;
+import java.util.List;
 
 public interface Database {
+
 
     public boolean createWF(String city, WeatherForecast weatherForecast);
 
     public WeatherForecast updateWF(String city, WeatherForecast wf);
 
     public WeatherForecast getWF(String city);
+
+    public List<WeatherForecast> getListOfWF(String city, int amountOfDays);
 
     public boolean deleteWF(String city);
 
--- src/main/java/com/charlieThao/weather_forcast_demo/repository/MemoryDatabase.java
+++ src/main/java/com/charlieThao/weather_forcast_demo/repository/MemoryDatabase.java
@@ -2,11 +2,13 @@ package com.charlieThao.weather_forcast_demo.repository;
 
 import com.charlieThao.weather_forcast_demo.model.WeatherForecast;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MemoryDatabase implements Database {
 
-    public HashMap<String, WeatherForecast> weatherForecastList = new HashMap<String, WeatherForecast>() {
+    public HashMap<String, WeatherForecast> weatherForecastList = new HashMap<>() {
         {
             put("New York", new WeatherForecast(26.0, 25.0, false, "cloudy", "New York"));
             put("Melbourn", new WeatherForecast(29.0, 20.0, true, "snowy", "Melbourn"));
@@ -37,6 +39,24 @@ public class MemoryDatabase implements Database {
     }
 
     @Override
+    public List<WeatherForecast> getListOfWF(String city, int amountOfDays) {
+//        List<WeatherForecast> wfList = new ArrayList<>();
+//        List<WeatherForecast> tmpList = (List<WeatherForecast>) weatherForecastList.values();
+//
+//        for (int i = 0; i < amountOfDays && i < tmpList.size(); i++) {
+//            wfList.add(tmpList.get(i));
+//        }
+//
+//        return wfList;
+
+//        return weatherForecastList.values().stream().filter(
+//                ele -> ele.getCity().equals(city)
+//        ).limit(amountOfDays).toList();
+
+        return weatherForecastList.values().stream().limit(amountOfDays).toList();
+    }
+
+    @Override
     public boolean deleteWF(String city) {
         if (weatherForecastList.containsKey(city)) {
             weatherForecastList.remove(city);
@@ -44,4 +64,6 @@ public class MemoryDatabase implements Database {
         }
         return false;
     }
+
+
 }
