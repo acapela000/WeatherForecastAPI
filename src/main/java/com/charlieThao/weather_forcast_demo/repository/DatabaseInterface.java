fb26e589fafd2c0729a85431d0fc14bc6c539609
thao <mrtrados@gmail.com>
2023-03-09 20:40:39 +0900

    ADD Repository annotation

--- src/main/java/com/charlieThao/weather_forcast_demo/repository/DatabaseInterface.java
+++ src/main/java/com/charlieThao/weather_forcast_demo/repository/DatabaseInterface.java
@@ -1,12 +1,14 @@
 package com.charlieThao.weather_forcast_demo.repository;
 
 import com.charlieThao.weather_forcast_demo.model.WeatherForecast;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DatabaseInterface <T, U> {
 
-    public boolean creat(U id, T item);
+    public boolean creat(T item);
 
     public T getById(U id);
 
