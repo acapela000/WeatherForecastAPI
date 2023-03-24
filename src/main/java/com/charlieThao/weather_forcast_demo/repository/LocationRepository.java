6d0f113dc8cb05aaa0e33c829d1226e6859cbcb7
thao <mrtrados@gmail.com>
2023-03-24 11:12:59 +0900

    ADD annotation CrossOrigin

--- src/main/java/com/charlieThao/weather_forcast_demo/repository/LocationRepository.java
+++ src/main/java/com/charlieThao/weather_forcast_demo/repository/LocationRepository.java
@@ -4,8 +4,10 @@ import com.charlieThao.weather_forcast_demo.model.Location;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "locationList", path = "location")
+@CrossOrigin("*")
 public interface LocationRepository extends CrudRepository<Location, String>, PagingAndSortingRepository<Location, String> {
 
 }
