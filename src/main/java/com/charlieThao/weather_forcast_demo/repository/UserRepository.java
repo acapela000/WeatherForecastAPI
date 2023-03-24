f894ef58bd14c6dacff42a02577f542f79f2363e
thao <mrtrados@gmail.com>
2023-03-24 18:01:38 +0900

    ADD findByUserName, ByEmail

--- src/main/java/com/charlieThao/weather_forcast_demo/repository/UserRepository.java
+++ src/main/java/com/charlieThao/weather_forcast_demo/repository/UserRepository.java
@@ -4,10 +4,18 @@ import com.charlieThao.weather_forcast_demo.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "userList", path = "user")
 @CrossOrigin("*")
 public interface UserRepository extends CrudRepository<User, String>, PagingAndSortingRepository<User, String> {
 
+    @RestResource(path = "/userName")
+    List<User> findByUserName(String userName);
+
+    @RestResource(path = "/email")
+    List<User> findByEmail(String email);
 }
