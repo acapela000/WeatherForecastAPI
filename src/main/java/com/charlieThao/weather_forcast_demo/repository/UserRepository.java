fcbbea21fcc6a0b7c71e23510499f38d176e11df
thao <mrtrados@gmail.com>
2023-03-24 11:25:18 +0900

    ADD annotation CrossOrigin

--- src/main/java/com/charlieThao/weather_forcast_demo/repository/UserRepository.java
+++ src/main/java/com/charlieThao/weather_forcast_demo/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.charlieThao.weather_forcast_demo.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "userList", path = "user")
+@CrossOrigin("*")
 public interface UserRepository extends CrudRepository<User, String>, PagingAndSortingRepository<User, String> {
+
 }
--- src/main/java/com/charlieThao/weather_forcast_demo/repository/WFRepository.java
+++ src/main/java/com/charlieThao/weather_forcast_demo/repository/WFRepository.java
@@ -4,9 +4,10 @@ import com.charlieThao.weather_forcast_demo.model.WeatherForecast;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "wfList", path = "wf")
+@CrossOrigin("*")
 public interface WFRepository extends CrudRepository<WeatherForecast, String>, PagingAndSortingRepository<WeatherForecast, String> {
-
 
 }
