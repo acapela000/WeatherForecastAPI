4576f0fde2e69c15a5bd40f0563e1477eb84dc2f
thao <mrtrados@gmail.com>
2023-03-24 18:02:09 +0900

    ADD findByPrecipitating, ByCondition, ByTemperature

--- src/main/java/com/charlieThao/weather_forcast_demo/repository/WFRepository.java
+++ src/main/java/com/charlieThao/weather_forcast_demo/repository/WFRepository.java
@@ -4,10 +4,21 @@ import com.charlieThao.weather_forcast_demo.model.WeatherForecast;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "wfList", path = "wf")
 @CrossOrigin("*")
 public interface WFRepository extends CrudRepository<WeatherForecast, String>, PagingAndSortingRepository<WeatherForecast, String> {
 
+    @RestResource(path = "/precipitating")
+    List<WeatherForecast> findByPrecipitating(boolean isPrecipitating);
+
+    @RestResource(path = "/condition")
+    List<WeatherForecast> findByCondition(String condition);
+
+    @RestResource(path = "/temperature")
+    List<WeatherForecast> findByTemperature(String temperature);
 }
